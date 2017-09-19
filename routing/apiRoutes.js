a465d3c29f571d3a2bb7f44521056a86222cbd44
Zachary Saltzman <zsaltzman@Zacharys-MacBook-Pro-2.local>
2017-09-19 15:16:35 -0400

    Prep for Heroku

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -8,8 +8,34 @@ module.exports = function(app) {
 
 	app.post("/api/friends", function (req, res) {
 		console.log(req.body);
-		friends.push(req.body);
-		res.json(true);
+		var user = req.body;
+		var userScores = user.scores;
+
+				var bestMatch = friends[0];
+				var bestScore = 100;
+				var compScore = 0;
+
+				for (var i in friends) {
+					for (var j = 0; j <10; j++) {
+						compScore += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
+						console.log(compScore);
+					}
+					if ((bestScore > compScore) && (user.name != friends[i].name)) {
+						bestMatch = friends[i];
+						bestScore = compScore;
+					}
+					compScore = 0;
+				}
+		var nameFlag = true;
+		for (var i = 0; i < friends.length; i++) {
+			if (user.name == friends[i].name) {
+				nameFlag = false;
+			}
+		}
+		if (nameFlag) {
+			friends.push(req.body);
+		}
+		res.json(bestMatch);
 	});
 
 }
